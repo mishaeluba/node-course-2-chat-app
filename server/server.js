934e47e9970542b7c6bda78ab02b64f5313ccffb
uba mishael <ubamishael@gmail.com>
2018-07-12 02:15:31 +0100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const publicpath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicpath));
+
+app.listen(3000, () => {
+    console.log('server is up on ${port}');
+
+});
